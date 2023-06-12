11951093e41b6b5d0ea768467b2c363fa0908e5b
Moe Montenegro <moe@senecafoundation.org>
2023-06-12 12:13:35 -0500

    Index

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
+import List from './components/List';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">      
         <Button />
+        <List />
       </header>
     </div>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,15 +2,29 @@ import React, { useState } from 'react';
 
 
 export default function Button () {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('Button pressed');
+  }
+
   const [count, setCount  ] = useState(0);
+
+  
 
   return (
     <div>
       <div>
-      { count } 
+      Count: { count } 
       </div>
+      
+      <form onSubmit={handleSubmit}>
+        <p>Submit - Prevent Default</p>
+        <button type="submit">Submit</button>
+      </form>
+
+      <p>Click Button</p>
       <button onClick={() => setCount(count + 1) }>
-        Add
+        Add 
       </button>
     </div>    
   );
