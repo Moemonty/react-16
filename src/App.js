1cb73456118523928199d19bad664b6f79d3d717
Moe Montenegro <moe@senecafoundation.org>
2023-06-12 16:50:20 -0500

    Add axios for async and useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
 import List from './components/List';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [message, setMessage] = useState('Loading');
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    loadMessage();
+  }, []);
+
+  const loadMessage = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get(
+        'https://json.versant.digital/.netlify/functions/fake-api/message'
+      );
+      setMessage(response.data);
+      setIsLoading(false);
+    } catch (e) {
+      setMessage(e.message);
+      setIsLoading(false);
+    }
+  };
+
+  if (isLoading) {
+    return 'Loading...';
+  }
+
   return (
     <div className="App">
-      <header className="App-header">      
+      <header className="App-header">
+        Async Message: { message }      
         <Button />
         <List />
       </header>
---
+++ src/components/List.js
@@ -0,0 +1,83 @@
+import react from "react";
+
+export default function List() {
+  const numbers = [1, 2, 3, 4, 5];
+  
+  // Function Expression
+  const listItems = numbers.map(function(number, index) {
+    return <li key={index}>{number}</li>; 
+  });
+
+  // Arrow Function
+  const arrowItems = numbers.map((number, index) => <li key={index}>{number}</li>);
+
+
+  // This with Arrow Functions
+  const arrowObj = {
+    name: "John",
+    sayHello: function() {
+      setTimeout(() => {
+        console.log("Hello, " + this.name);
+      }, 1000);
+    }
+  };
+  
+  
+  // this without arrow functions
+  const funcObj = {
+    name: "John",
+    sayHello: function() {
+      setTimeout(function() {
+        console.log("Hello, " + this.name);
+      }.bind(this), 1000);
+    }
+  };
+ 
+  // this without arrow functions unbound this
+  const funcObjUnbound = {
+    name: "John",
+    sayHello: function() {
+      window.name = 'Global Name';
+      setTimeout(function() {
+        console.log("Hello, " + this.name);
+      }, 1000);
+    }
+  };
+
+  const arrowFunc = () => {
+    arrowObj.sayHello();
+  }
+
+  const callFunc = () => {
+    funcObj.sayHello();
+  }
+ 
+  const callFuncUnbound = () => {
+    funcObjUnbound.sayHello();
+  }
+
+  return (
+    <div>
+      <h2>List Items</h2>
+      <ul>
+        {listItems}
+      </ul>
+      <h2>Arrow Items</h2>
+      <ul>
+        { arrowItems}
+      </ul>
+
+      <button onClick={arrowFunc}>
+        Arrow Function
+      </button>
+
+      <button onClick={callFunc}>
+        Function Bind
+      </button>
+
+      <button onClick={callFuncUnbound}>
+        Function Unbound This
+      </button>
+    </div>    
+  );
+}
