cabfde71ca34ca25b726c93cc9f4efd0cfc92582
Moe Montenegro <moe@senecafoundation.org>
2023-08-23 16:02:01 -0500

    Revert changes

---
+++ src/components/Stateapp.js
@@ -0,0 +1,38 @@
+// import '../App.css';
+//
+// export const StoreContext = React.createContext([]);
+//
+// const initialState = {
+//   profile: null,
+//   greeting: '',
+//   content: []
+// };
+//
+// const reducer = (state, action) => {
+//   switch(action.type) {
+//     case 'REFRESH_GREETING':
+//       // return updated state
+//     case 'REFRESH_PROFILE':
+//       return {
+//         //state
+//       }
+//     default:
+//       return state;
+//   }
+// }
+//
+// // set up state and dispatch function
+// const [globalState, dispatch] = useReducer(reducer, initialState);
+//
+// function Stateapp() {
+//   return (
+//     <StoreContext.Provider>
+//       <div className="App">
+//         Set Greeting:
+//         <input value={}>
+//       </div>
+//     </StoreContext.Provider>
+//   )
+// }
+//
+// export default Stateapp;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Usestateprop from "./components/Usestateprop";
+import Stateapp from "./components/Stateapp";
+
+// Comment out to see different apps
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Usestateprop/>, document.getElementById('root-name'));
+// ReactDOM.render(<Usestateprop/>, document.getElementById('root-name'));
+// ReactDOM.render(<Stateapp/>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
